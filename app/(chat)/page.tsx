07d38c8feca0c441fb300c6a1f2d5570395ab8ef
ray <uubs@yeah.net>
2024-05-23 11:01:45 +0800

    update chatbot

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -6,7 +6,7 @@ import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
 
 export const metadata = {
-  title: 'Learning language'
+  title: 'AI外语通'
 }
 
 export default async function IndexPage() {
