3419d18e0140e31bf0e4b5e45f35c591dcf10830
ray <uubs@yeah.net>
2024-05-29 19:33:20 +0800

    update chatbot

--- components/chat.tsx
+++ components/chat.tsx
@@ -98,8 +98,6 @@ export function Chat({ id, className, session, missingKeys }: ChatProps) {
     if (session?.user) {
       if (!path.includes('chat') && messages.length === 2) {
         window.history.replaceState({}, '', `/learninglang/chat/${id}`)
-        // window.localStorage.setItem('tts', true);
-        router.refresh()
       }
     }
 
