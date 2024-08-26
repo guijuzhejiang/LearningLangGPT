55586f2456096d0ff365620f59c86de774e418f4
ray <uubs@yeah.net>
2024-08-26 15:16:10 +0800

    update models

--- i18n.ts
+++ i18n.ts
@@ -8,6 +8,6 @@ export default getRequestConfig(async () => {
 
     return {
         locale,
-        messages: (await import(`./lang/${locale}.json`)).default
+        messages: (await import(`@/lang/${locale}.json`)).default
     };
 });
