6f68306c70f682680aa1d3dba6eeb50560f8f004
ray <uubs@yeah.net>
2024-08-26 15:30:50 +0800

    update models

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ module.exports = withNextIntl({
   // output: 'export',
   experimental: {
     serverActions: {
-      allowedOrigins: ['*'],
+      allowedForwardedHosts: ['zs.imegaai.com'],
+      allowedOrigins: ['zs.imegaai.com'],
     },
   },
   env: {
