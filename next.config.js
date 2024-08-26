d2beb1201123e52ddc1d2afb79eb85b178c0bbb3
ray <uubs@yeah.net>
2024-08-26 15:05:30 +0800

    update models

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = withNextIntl({
   // output: 'export',
   experimental: {
     serverActions: {
-      allowedOrigins: process.env.NODE_ENV === "development"?[]:['aidu.org.cn'],
+      allowedOrigins: process.env.NODE_ENV === "development"?[]:['imegaai.com'],
     },
   },
   env: {
